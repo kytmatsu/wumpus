83e3732eba347c7ee45718501a385f535772a8f4
kytmatsu <kyletmatsumoto@gmail.com>
2022-02-11 14:20:23 -0800

    stuff added

--- wump/src/app/app.component.ts
+++ wump/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   ngOnInit(): void {
     this.placeStuff();
     this.element = document.getElementById('print-wump');
+    this.textbox = document.getElementById('textbox');
     this.inspectCurrentRoom();
     //this.showChoices();
   }
@@ -37,12 +38,13 @@ export class AppComponent {
   startingRoom = 0;
   wumpusStart = 0;
   answer = '';
-  element = document.getElementById('print-wump');
+  myAns = '';
+  element: any;
+  textbox: any;
   rooms:number[][] = [[1, 4, 7], [0, 2, 9], [1, 3, 11], [2, 4, 13], [0, 3, 5],
   [4, 6, 14], [5, 7, 16], [0, 6, 8], [7, 9, 17], [1, 8, 10],
   [9, 11, 18], [2, 10, 12], [11, 13, 19], [3, 12, 14], [5, 13, 15],
   [14, 16, 19], [6, 15, 17], [8, 16, 18], [10, 17, 19], [12, 15, 18]];
-
 
   rand(min:number,max:number) {
     min = Math.ceil(min);
@@ -78,28 +80,22 @@ export class AppComponent {
     return true;
   }
 
-  placePlayer() {
-    this.numArrows = 3;
-    this.playerAlive = true;
-    this.startingRoom = 0;
-    this.currentRoom = this.move(0);
-  }
-
  async parseAnswer()
   {
     //alert(myAns);
-    var myAns = this.answer.replace(/[\s]/g,'');
+    var myAnswer = this.answer.replace(/[\s]/g,'');
+    this.myAns = myAnswer;
     if(this.playerAlive == true){
-    switch(myAns)
+    switch(myAnswer)
               {
                   case 'q':
-                      this.performAction(myAns);
+                      this.performAction(myAnswer);
                       break;
                   case 'w':
-                      this.performAction(myAns);
+                      this.performAction(myAnswer);
                       break;
                   case 'e':
-                      this.performAction(myAns);
+                      this.performAction(myAnswer);
                       break;
                   default:
                     if(this.waitTime == true){
@@ -112,7 +108,140 @@ export class AppComponent {
                       break;
               }
             }
+            
+    this.textbox.focus();
+    this.answer = '';
+    
   }
+
+  async performAction(ans:String) {
+    //var myAns = this.convertStringToNumber(this.answer);
+    debugger;
+    switch(ans)
+    {
+        case 'q':
+          var temp = 0;
+          this.waitTime = false;
+            if(this.element){
+              this.element.textContent = "Which room?\r\n";
+              for (var j = 0; j < 3; j++)
+              {
+                this.element.textContent += this.rooms[this.currentRoom][j] + " ";
+              }
+            }
+            while(temp == 0){
+            //this.waitInput();
+            this.unlisten = this.renderer.listen(this.butClick.nativeElement, 'click', (event)=>{this.waitResolve()});
+              await this.waitForPress();
+            var newAns = this.convertStringToNumber(this.myAns.replace(/[^\/\d]/g,''));
+            alert("newAns " + newAns);
+                if (this.isValidMove(newAns))
+                {
+                    this.currentRoom = this.move(newAns);
+                    temp++;
+                    if(this.element){
+                      this.element.textContent = "";
+                    }
+                    this.inspectCurrentRoom();
+                }
+                else
+                {
+                  if(this.element){
+                    this.element.textContent = "You cannot move there.\r\n";
+                    this.element.textContent += "Valid Rooms are:\r\n";
+                    for (var j = 0; j < 3; j++)
+                    {
+                      this.element.textContent += this.rooms[this.currentRoom][j] + " ";
+                    }
+                  }
+                }
+              }
+              this.unlisten();
+            break;
+        case 'w':
+            if(this.numArrows > 0){
+              var temp = 0;
+              this.waitTime = false;
+              if(this.element){
+                this.element.textContent = "Which room?\r\n";
+                for (var j = 0; j < 3; j++)
+                {
+                  this.element.textContent += this.rooms[this.currentRoom][j] + " ";
+                }
+            }
+              while(temp == 0){
+              //this.waitInput();
+              this.unlisten = this.renderer.listen(this.butClick.nativeElement, 'click', (event)=>{this.waitResolve()});
+                await this.waitForPress();
+              var newAns = this.convertStringToNumber(this.myAns.replace(/[^\/\d]/g,''));  
+                    if (this.isValidMove(newAns))
+                    {
+                      temp++;
+                        this.numArrows -= 1;
+                        if(newAns == this.wumpusRoom){
+                          if(this.element){
+                            this.element.textContent = "You hear a noise: 'GRAWR' ... Splat!\r\n";
+                          }
+                            this.wumpusAlive = false;
+                            this.showChoices();
+                        }
+                        else
+                        {
+                          if(this.element){
+                            this.element.textContent = "Miss!\r\n"
+                            this.element.textContent += "Arrows Left: " + this.numArrows;
+                            //this.element.textContent += this.numArrows;
+                          }
+                          this.startleWumpus(this.wumpusRoom);
+                            if(this.wumpusRoom == this.currentRoom){
+                              if(this.element){
+                                this.element.textContent = "The wumpus attacked you! You've been killed.\r\n";
+                              }
+                                this.playerAlive = false;
+                                this.showChoices();
+                            }
+
+                        }
+                        if(this.numArrows <= 0 && this.wumpusAlive == true){
+                          if(this.element){
+                            this.element.textContent = "You do not have any arrows left!\r\n" + "You have no choice but to accept your fate.\r\n";
+                            this.playerAlive = false;
+                            this.showChoices();
+                          }
+                        }
+                        this.inspectCurrentRoom();
+                    }
+                    else
+                    {
+                      if(this.element){
+                        this.element.textContent = "You cannot shoot there.\r\n";
+                        this.element.textContent += "Valid Rooms are:\r\n";
+                        for (var j = 0; j < 3; j++)
+                        {
+                           this.element.textContent += this.rooms[this.currentRoom][j] + " ";
+                        }
+                      }
+                    }
+                    this.unlisten();
+            } 
+            //this.unlisten();
+          }
+            break;
+        case 'e':
+            if(this.element){
+            this.element.textContent = "Quitting the current game.\r\n";
+            }
+            this.playerAlive = false;
+            this.showChoices();
+            break;
+        default:
+          if(this.element){
+            this.element.textContent = "You cannot do that. You can move, shoot, or quit."
+          }
+          this.waitTime = false;
+            break;
+          }
+}
 
   showChoices(){
     if(this.playerAlive && this.wumpusAlive && this.numArrows > 0){
@@ -244,132 +373,7 @@ export class AppComponent {
               await this.waitForPress();
     }*/
 
-    async performAction(ans:String) {
-    //var myAns = this.convertStringToNumber(this.answer);
-    debugger;
-    switch(ans)
-    {
-        case 'q':
-          var temp = 0;
-          this.waitTime = false;
-            if(this.element){
-              this.element.textContent = "Which room?\r\n";
-              for (var j = 0; j < 3; j++)
-              {
-                this.element.textContent += this.rooms[this.currentRoom][j] + " ";
-              }
-            }
-            while(temp == 0){
-            //this.waitInput();
-            this.unlisten = this.renderer.listen(this.butClick.nativeElement, 'click', (event)=>{this.waitResolve()});
-              await this.waitForPress();
-            var newAns = this.convertStringToNumber(this.answer.replace(/[^\/\d]/g,''));
-            //alert("newAns " + newAns);
-                if (this.isValidMove(newAns))
-                {
-                    this.currentRoom = this.move(newAns);
-                    temp++;
-                    this.inspectCurrentRoom();
-                }
-                else
-                {
-                  if(this.element){
-                    this.element.textContent = "You cannot move there.\r\n";
-                    this.element.textContent += "Valid Rooms are:\r\n";
-                    for (var j = 0; j < 3; j++)
-                    {
-                      this.element.textContent += this.rooms[this.currentRoom][j] + " ";
-                    }
-
-                  }
-                }
-              }
-              this.unlisten();
-            break;
-        case 'w':
-            if(this.numArrows > 0){
-              var temp = 0;
-              this.waitTime = false;
-              if(this.element){
-                this.element.textContent = "Which room?\r\n";
-                for (var j = 0; j < 3; j++)
-                {
-                  this.element.textContent += this.rooms[this.currentRoom][j] + " ";
-                }
-            }
-              while(temp == 0){
-              //this.waitInput();
-              this.unlisten = this.renderer.listen(this.butClick.nativeElement, 'click', (event)=>{this.waitResolve()});
-                await this.waitForPress();
-              var newAns = this.convertStringToNumber(this.answer.replace(/[^\/\d]/g,''));  
-                    if (this.isValidMove(newAns))
-                    {
-                      temp++;
-                        this.numArrows -= 1;
-                        if(newAns == this.wumpusRoom){
-                          if(this.element){
-                            this.element.textContent = "You hear a noise: 'GRAWR' ... Splat!\r\n";
-                          }
-                            this.wumpusAlive = false;
-                            this.showChoices();
-                        }
-                        else
-                        {
-                          if(this.element){
-                            this.element.textContent = "Miss!\r\n"
-                            this.element.textContent += "Arrows Left: " + this.numArrows;
-                            //this.element.textContent += this.numArrows;
-                          }
-                          this.startleWumpus(this.wumpusRoom);
-                            if(this.wumpusRoom == this.currentRoom){
-                              if(this.element){
-                                this.element.textContent = "The wumpus attacked you! You've been killed.\r\n";
-                              }
-                                this.playerAlive = false;
-                                this.showChoices();
-                            }
-
-                        }
-                        if(this.numArrows <= 0 && this.wumpusAlive == true){
-                          if(this.element){
-                            this.element.textContent = "You do not have any arrows left!\r\n" + "You have no choice but to accept your fate.\r\n";
-                            this.playerAlive = false;
-                            this.showChoices();
-                          }
-                        }
-                        this.inspectCurrentRoom();
-                    }
-                    else
-                    {
-                      if(this.element){
-                        this.element.textContent = "You cannot shoot there.\r\n";
-                        this.element.textContent += "Valid Rooms are:\r\n";
-                        for (var j = 0; j < 3; j++)
-                        {
-                           this.element.textContent += this.rooms[this.currentRoom][j] + " ";
-                        }
-                      }
-                    }
-                    this.unlisten();
-            } 
-            //this.unlisten();
-          }
-            break;
-        case 'e':
-            if(this.element){
-            this.element.textContent = "Quitting the current game.\r\n";
-            }
-            this.playerAlive = false;
-            this.showChoices();
-            break;
-        default:
-          if(this.element){
-            this.element.textContent = "You cannot do that. You can move, shoot, or quit."
-          }
-          this.waitTime = false;
-            break;
-          }
-}
+    
 
 startleWumpus(newRoom: number){
     var random = this.rand(1,5) % 4;
@@ -381,6 +385,13 @@ startleWumpus(newRoom: number){
     }
 }
 
+placePlayer() {
+  this.numArrows = 3;
+  this.playerAlive = true;
+  this.startingRoom = 0;
+  this.currentRoom = this.move(0);
+}
+
 placePits() {
   this.pitRoom1 = this.rand(1,20) % 20;
   this.pitRoom2 = this.rand(1,20) % 20;
@@ -390,6 +401,7 @@ placePits() {
   }
 }
 placeWumpus() {
+  this.wumpusAlive = true;
   var randomRoom = this.rand(1,20) % 20;
   this.wumpusRoom = randomRoom;
   this.wumpusStart = this.wumpusRoom;
@@ -422,18 +434,25 @@ placeStuff(){
 }
 
 async playAgain(){
+  debugger;
   if(this.element){
     this.element.textContent +=  "Would you like to play again? \r\n'Y?' \r\n";
   }
   this.unlisten = this.renderer.listen(this.butClick.nativeElement, 'click', (event)=>{this.waitResolve()});
               await this.waitForPress();
-    if(this.answer.toLowerCase() == 'y'){
-      if(this.element){
+    if(this.myAns.toLowerCase() == 'y')
+    {
+      if(this.element)
+      {
         this.element.textContent =  "";
       }
-        this.placeStuff();
-        this.inspectCurrentRoom();
-    } else {
+        //this.placeStuff();
+        //this.inspectCurrentRoom();
+        
+        window.location.reload();
+    } 
+    else 
+    {
       if(this.element){
         this.element.textContent =  "Thanks for playing!!";
       }
